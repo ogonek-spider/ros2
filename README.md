b6ca86740b96276f524f3f90faa2f07c3d89db47
alarin <me@alarin.ru>
2025-11-14 22:00:56 +0300

    spider-description from onshape export and rviz2 loader

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ https://robostack.github.io/GettingStarted.html
 
 pixi shell -e humble
 source install/local_setup.zsh 
+
+
+inspired by https://github.com/odriverobotics/ros_odrive/tree/main
+https://control.ros.org/master/doc/ros2_control_demos/example_7/doc/userdoc.html#
+
+
+# Robot description
+
+onshape -> ros urdf https://onshape-to-robot.readthedocs.io/en/latest/design.html#workflow-overview
---
+++ spider-description/launch/view_robot.launch.py
@@ -0,0 +1,45 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    urdf_path = os.path.join(
+        get_package_share_directory('spider-description'),  # Change to your package
+        'robot.urdf')
+    
+    with open(urdf_path, 'r') as infp:
+        robot_desc = infp.read()
+
+    # If you are using Xacro, you can use this instead:
+    # robot_desc = Command(['xacro ', urdf_path])
+
+    return LaunchDescription([
+        # Node to publish the robot's state (positions of all joints and links)
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_desc}],
+        ),
+
+        # Node to publish the state of non-fixed joints (opens a GUI for control)
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui'
+        ),
+
+        # Node to start RViz2
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=["-d", 'spider.rviz'],
+        ),
+    ])
